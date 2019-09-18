b3ff98eb1753c1a5cd5d5caf87fce9ac70ce8078
sunpengkai <3066969054@qq.com>
2019-09-18 17:27:50 +0800

    'shouye'

--- lottery/src/router.js
+++ lottery/src/router.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
-
 Vue.use(Router)
 
 export default new Router({
@@ -12,6 +11,6 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home
-    }
+    },
   ]
 })
