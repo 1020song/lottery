0987442557d26ca12a079cd87af1a7180cda2b3a
songyuhong <2814277263@qq.com>
2019-09-19 19:53:25 +0800

    gai

--- lottery/src/views/lotteryHall.vue
+++ lottery/src/views/lotteryHall.vue
@@ -93,7 +93,7 @@
 	}
 	.tab>div{
 		flex: 1;
-		line-height: .46875rem /* 30/64 */;
+		line-height: .625rem /* 40/64 */;
 		text-align: center;
 	}
 	.tit{
@@ -112,7 +112,6 @@
 	.tit dl dd{
 		font-size: .46875rem /* 30/64 */
 	}
-
 	.tit dl{
 		display: inline-block;
 		width: 50%;
