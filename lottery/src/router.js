20251fe398974af578b64fe6c2bad68b8decb9a9
Baoqianqian <2107624791@qq.com>
2019-09-19 09:47:59 +0800

    help&sontinue

--- lottery/src/router.js
+++ lottery/src/router.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
 
+import continues from './components/continue.vue' //继续选号
+import help from './components/help.vue' //帮助
+
 Vue.use(Router)
 
 export default new Router({
@@ -14,12 +17,14 @@ export default new Router({
       component: Home
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      path: '/help',
+      name: 'help',
+      component: help
+    },
+    {
+      path: '/continues',
+      name: 'continues',
+      component: continues
     }
   ]
 })
