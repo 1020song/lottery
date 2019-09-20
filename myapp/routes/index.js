b12e801b3d3b8b7a261ef526c0ba85ed8d20a7bc
songyuhong <2814277263@qq.com>
2019-09-20 14:40:46 +0800

    gai

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var fs =  require('fs')
+var cheerio=require('cheerio')
+var https=require('https')
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+
+
 
 module.exports = router;
